622d4629596d7c8e64f1873754c5976b53c88e4d
Laura <126280324+LauraAllObe@users.noreply.github.com>
2024-02-26 11:53:11 -0500

    added docstrings

--- Get_GitHub_commits.py
+++ Get_GitHub_commits.py
@@ -1,8 +1,29 @@
+"""
+This file contains code utilizing a REST-based API that takes in a user's GitHub 
+user ID and output's a list of the names of the repositories the user has along 
+with number of commits that are in each of the repositories. 
+Functions: get_user_repo_commits
+Libraries used: requests, json
+"""
 import requests
 import json
 
 
 def get_user_repo_commits(user_id):
+    """
+    Prints the the user's repositories and commits in each repository. 
+    If the response status code when fetching the user's repositories
+    is not 200, an error message is displayed. Else, iterate through each
+    repository. If the response status code when fetching the user's 
+    repository's commits is not 200, an error message is displayed. Else, 
+    the repository and number of commits is displayed on a single line.
+    Args:
+        user_id: string value of the user's GitHub ID
+    Returns:
+        N/A
+    Raises:
+        N/A
+    """
     # GitHub API endpoint for user repositories
     repo_url = f'https://api.github.com/users/{user_id}/repos'
 
