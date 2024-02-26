54ff307ff38cce9c608fc17c65f0f03c363294c0
Siddharth <ksid.kaza41@gmail.com>
2024-02-26 17:22:23 -0500

    Added test cases for github commits

---
+++ test_github.py
@@ -0,0 +1,21 @@
+import pytest
+from Get_GitHub_commits import get_user_repo_commits
+
+def test_user_github_commits(capsys):
+    user_id='' #enter the name of a user
+    get_user_repo_commits(user_id)
+    out,err=capsys.readouterr()
+    assert "Repo:" in out, "Output should list all repos"
+    assert "Number of commits:" in out, "Output should list the number of commits"
+
+def test_invalid_user(capsys):
+    user_id=' nonexistent_user'
+    get_user_repo_commits(user_id)
+    out,err=capsys.readouterr()
+    assert "Error" in out, "Output should indicate an error for invalid or nonexisting user"
+
+def test_user_with_no_repos(capsys):
+    user_id='' #enter the name of a user
+    get_user_repo_commits(user_id)
+    out ,err=capsys.readouterr()
+    assert out == "", "Expected no output for a user with no repositories or a non-existent user"
