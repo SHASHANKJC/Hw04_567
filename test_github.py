b592878291e71bce4710af2fe61bb566ba36cd6f
Siddharth <ksid.kaza41@gmail.com>
2024-02-26 19:12:16 -0500

    Added example user id's

--- test_github.py
+++ test_github.py
@@ -2,7 +2,7 @@ import pytest
 from Get_GitHub_commits import get_user_repo_commits
 
 def test_user_github_commits(capsys):
-    user_id='' #enter the name of a user
+    user_id='sidhdhuk09'
     get_user_repo_commits(user_id)
     out,err=capsys.readouterr()
     assert "Repo:" in out, "Output should list all repos"
@@ -15,7 +15,7 @@ def test_invalid_user(capsys):
     assert "Error" in out, "Output should indicate an error for invalid or nonexisting user"
 
 def test_user_with_no_repos(capsys):
-    user_id='' #enter the name of a user
+    user_id='tushar2808'
     get_user_repo_commits(user_id)
     out ,err=capsys.readouterr()
     assert out == "", "Expected no output for a user with no repositories or a non-existent user"
